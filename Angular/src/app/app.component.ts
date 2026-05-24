412da70169225fed629f34a8883ba9bbb536dfc6
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-24 12:19:34 +0300

    Angular - Update to use standalone components

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import notify from 'devextreme/ui/notify';
 
+import { DxTextAreaModule } from 'devextreme-angular/ui/text-area';
+
 @Component({
     selector: 'app-root',
+    imports: [DxTextAreaModule],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    standalone: false
 })
 export class AppComponent {
   longText = 'Japan is a sovereign island nation in East Asia. Located in the Pacific Ocean, it lies off the eastern coast of the Asian mainland and stretches from the Sea of Okhotsk in the north to the East China Sea and China in the southwest. The Greater Tokyo Area is the most populous metropolitan area in the world.';
