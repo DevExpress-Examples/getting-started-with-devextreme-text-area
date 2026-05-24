d8b965e62a5c682ff74fe00d76de551e869ae96e
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-24 12:18:22 +0300

    chore(angular): ng update 18 → 19

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import notify from 'devextreme/ui/notify';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   longText = 'Japan is a sovereign island nation in East Asia. Located in the Pacific Ocean, it lies off the eastern coast of the Asian mainland and stretches from the Sea of Okhotsk in the north to the East China Sea and China in the southwest. The Greater Tokyo Area is the most populous metropolitan area in the world.';
